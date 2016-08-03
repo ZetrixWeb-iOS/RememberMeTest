f037246f2e52653723254c2476f3fd6d0786e780
cloudjanak <cloudjanak@gmail.com>
2016-08-03 14:26:19 +0530

    First Commit

---
+++ RememberMe-iOS-master/RemMe/RemMe/Models/Item.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using SQLite;
+using SQLite.Net.Attributes;
+
+namespace RememberMe.Models
+{
+	public class Item
+	{
+		public Item ()
+		{
+		}
+
+		[PrimaryKey, AutoIncrement]
+		public int ID { get; set; }
+		public string Name { get; set; }
+		public string Place { get; set; }
+        
+        [Ignore]
+        public string NamePlace
+        {
+            get
+            {
+                return "Your " + Name + " is " + Place;
+            }
+        }
+    }
+}
+
---
+++ RememberMe-iOS-master/RemMe/RemMe/Pages/SearchPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using RememberMe.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace RememberMe.Pages
+{
+    public partial class SearchPage : ContentPage
+    {
+
+        private string _search;
+        private List<Item> _items;
+
+        public SearchPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            Search = "";
+        }
+
+        public void SearchClicked(object sender, EventArgs e)
+        {
+            SearchItems();
+        }
+
+        public void OnDeleteItem(object sender, EventArgs e)
+        {
+            App.Database.DeleteItem(((sender as BindableObject).BindingContext as Item).ID);
+            SearchItems();
+        }
+
+        public void OnItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            App.Current.Navigation.PushAsync(new DetailPage(e.Item as Item));
+        }
+
+        public string Search
+        {
+            get
+            {
+                return _search;
+            }
+
+            set
+            {
+                _search = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public void SearchItems()
+        {
+            Items = App.Database.GetItems(Search).ToList();
+        }
+
+        public List<Item> Items
+        {
+            get
+            {
+                return _items;
+            }
+
+            set
+            {
+                _items = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
